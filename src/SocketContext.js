807a7217bcdf4efa08185130ae21169f24b5ab98
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-11-22 22:12:04 +0700

    Revert "fix not show hang up from answer side"
    This reverts commit fd885ef9882e6a2e6d3629f8fa97d6bc32dd7526.
    
    rollback

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -36,8 +36,6 @@ const ContextProvider = ({ children }) => {
         socket.on("callEnded", () => {
             leaveCall();
         });
-
-        setCallEnded(false);
     }, []);
 
     const answerCall = () => {
